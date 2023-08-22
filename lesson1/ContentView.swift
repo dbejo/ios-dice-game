7520c73d9074a3b14fc385facc30e613baed46db
dbejo <bejodavid123@gmail.com>
2023-08-22 20:12:48 +0200

    Add support for night mode

--- lesson1/ContentView.swift
+++ lesson1/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
     @State var number = 6
     @State var isPressed = false
 
@@ -19,7 +20,7 @@ struct ContentView: View {
                 Spacer()
             }.padding()
             ZStack{
-                Rectangle().fill(.white)
+                Rectangle().fill(colorScheme == .dark ? Color(red: 0.1, green: 0.1, blue: 0.1) : Color(red: 0.93, green: 0.93, blue: 0.93)).cornerRadius(15)
                 if(isPressed) {
                     Image(systemName: "die.face." + String(number)).resizable()
                         .aspectRatio(contentMode: .fit)
@@ -34,18 +35,19 @@ struct ContentView: View {
             Button {
                 throwDice()
             } label: {
-                Text("THROW")
+                Text("THROW").foregroundColor(colorScheme == .dark ? .white : .black)
             }
             .foregroundColor(.black)
             .font(.title)
             .bold()
             .padding()
             .background(Rectangle()
-                .fill(.white)
+                .fill(colorScheme == .dark ? Color(red: 0.05, green: 0.05, blue: 0.05) : Color(red: 0.95, green: 0.95, blue: 0.95))
                 .cornerRadius(15)
                 .shadow(radius: 10))
             Spacer()
         }
+        .background(Rectangle().fill(colorScheme == .dark ? Color(red: 0.15, green: 0.15, blue: 0.15) : Color(red: 0.9, green: 0.9, blue: 0.9)).ignoresSafeArea())
     }
     
     func throwDice() {
