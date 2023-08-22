5c64396130db28ed4c2a520b85d190a3b03f420f
dbejo <david.bejo@genesys.com>
2023-08-22 17:07:39 +0200

    Initial Commit

---
+++ lesson1/lesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson1App.swift
+//  lesson1
+//
+//  Created by Bejó Dávid on 2023. 08. 22..
+//
+
+import SwiftUI
+
+@main
+struct lesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
