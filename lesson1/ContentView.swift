29f4eaec001c35f919edbd87348f0c796f887cc2
dbejo <david.bejo@genesys.com>
2023-08-22 19:07:22 +0200

    Add dice throwing game

--- lesson1/ContentView.swift
+++ lesson1/ContentView.swift
@@ -8,19 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number = 6
+    @State var isPressed = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("Throw a dice")
+                    .font(.largeTitle)
+                Spacer()
+            }.padding()
+            ZStack{
+                Rectangle().fill(.white)
+                if(isPressed) {
+                    Image(systemName: "die.face." + String(number)).resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding()
+                } else {
+                    Text("Let's play!")
+                        .font(.largeTitle)
+                        .bold()
+                }
+            }
+            .padding()
+            Button {
+                throwDice()
+            } label: {
+                Text("THROW")
+            }
+            .foregroundColor(.black)
+            .font(.title)
+            .bold()
+            .padding()
+            .background(Rectangle()
+                .fill(.white)
+                .cornerRadius(15)
+                .shadow(radius: 10))
+            Spacer()
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func throwDice() {
+        isPressed = true
+        number = Int.random(in: 1...6)
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
