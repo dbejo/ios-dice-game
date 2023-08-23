e4b1ca1f31e778c096048e48435d9436d0391848
dbejo <bejodavid123@gmail.com>
2023-08-23 07:49:12 +0200

    Assign colours to variable

--- lesson1/ContentView.swift
+++ lesson1/ContentView.swift
@@ -11,6 +11,12 @@ struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
     @State var number = 6
     @State var isPressed = false
+    var dark1 = Color(red: 0.05, green: 0.05, blue: 0.05)
+    var dark2 = Color(red: 0.1, green: 0.1, blue: 0.1)
+    var dark3 = Color(red: 0.15, green: 0.15, blue: 0.15)
+    var light1 = Color(red: 0.9, green: 0.9, blue: 0.9)
+    var light2 = Color(red: 0.93, green: 0.93, blue: 0.93)
+    var light3 = Color(red: 0.95, green: 0.95, blue: 0.95)
 
     var body: some View {
         VStack {
@@ -20,7 +26,7 @@ struct ContentView: View {
                 Spacer()
             }.padding()
             ZStack{
-                Rectangle().fill(colorScheme == .dark ? Color(red: 0.1, green: 0.1, blue: 0.1) : Color(red: 0.93, green: 0.93, blue: 0.93)).cornerRadius(15)
+                Rectangle().fill(colorScheme == .dark ? dark2 : light2).cornerRadius(15)
                 if(isPressed) {
                     Image(systemName: "die.face." + String(number)).resizable()
                         .aspectRatio(contentMode: .fit)
@@ -42,12 +48,12 @@ struct ContentView: View {
             .bold()
             .padding()
             .background(Rectangle()
-                .fill(colorScheme == .dark ? Color(red: 0.05, green: 0.05, blue: 0.05) : Color(red: 0.95, green: 0.95, blue: 0.95))
+                .fill(colorScheme == .dark ? dark3 : light3)
                 .cornerRadius(15)
                 .shadow(radius: 10))
             Spacer()
         }
-        .background(Rectangle().fill(colorScheme == .dark ? Color(red: 0.15, green: 0.15, blue: 0.15) : Color(red: 0.9, green: 0.9, blue: 0.9)).ignoresSafeArea())
+        .background(Rectangle().fill(colorScheme == .dark ? dark1 : light1).ignoresSafeArea())
     }
     
     func throwDice() {
